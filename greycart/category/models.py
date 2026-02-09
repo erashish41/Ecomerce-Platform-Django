0a1de21bed79a3c2f13cbbabf4c56d68b5f528b8
Ashish Bhardwaj <erashish41@gmail.com>
2026-02-09 14:48:44 +0530

    initial setup with greycart

---
+++ greycart/category/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from utils.models import BaseMixin
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+
+CATEGORY_TYPE = (
+    ('cloth', 'Cloth'),
+    ('shoe', 'Shoe'),
+    ('utils', 'Utils'),   
+)
+
+class Category(BaseMixin):
+    name = models.CharField(max_length=150,unique=True)
+    description = models.TextField(blank=True)
+    category_type = models.CharField(choices=CATEGORY_TYPE, max_length=20)
+    image = CloudinaryField('image', blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    
+    class Meta:
+        ordering = ["name"]
+    
+    def __str__(self):
+        return self.name
+    
+    
+class Product(BaseMixin):
+    name = models.CharField(max_length=150,unique=True)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="products")
+    manufacture_date = models.DateField(auto_now_add=True)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    image = CloudinaryField('image',blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    
+    class Meta:
+        ordering = ["name"]
+        
+    def __str__(self):
+        return self.name
+    
+
---
+++ greycart/utils/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+
+class BaseMixin(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
