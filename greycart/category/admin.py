50912fde57ee44205ce7cb6e7c9cafd9c750481b
Ashish Bhardwaj <erashish41@gmail.com>
2026-02-17 22:11:52 +0530

    category updated

--- greycart/category/admin.py
+++ greycart/category/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from category.models import Category, Product
+from category.models import Category
 from django.contrib.admin import ModelAdmin
 
 # Register your models here.
@@ -11,8 +11,3 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ['name','category_type','is_active']
     
     
-@admin.register(Product)
-class ProductAdmin(admin.ModelAdmin):
-    prepopulated_fields = {"slug": ("name",)}
-    list_display = ['name','slug','description','category','price','is_active']
-    list_filter = ['name','category','is_active','price']
--- greycart/category/models.py
+++ greycart/category/models.py
@@ -29,21 +29,3 @@ class Category(BaseMixin):
     
     def __str__(self):
         return self.name
-    
-    
-class Product(BaseMixin):
-    name = models.CharField(max_length=150,unique=True)
-    slug = models.SlugField(max_length=120, unique=True, blank=True)
-    description = models.TextField(blank=True)
-    category = models.ForeignKey(Category,on_delete=models.CASCADE,related_name="products")
-    manufacture_date = models.DateField(auto_now_add=True)
-    price = models.DecimalField(max_digits=10,decimal_places=2)
-    image = CloudinaryField('image',blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    
-    class Meta:
-        ordering = ["name"]
-        
-    def __str__(self):
-        return self.name
-    
